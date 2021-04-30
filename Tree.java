496c833b2b5e537d0934891ab863a1bd666565e7
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2021-04-30 17:05:09 +0600

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    // This small task program will print a Tree
+    System.out.println("Hey there, I'm Emon and This is my very First project in Java'");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("  * ** *");
+    System.out.println("  ******");
+    System.out.println("   **** ");
+    System.out.println("    **  ");
+    System.out.println("    **  ");
+    System.out.println("~~~~~~~~~~");   
+  }
+  
+}
